d1552cf62cbea46166fefa7fdf8584ca45dc5604
gmelon <hsh1769@naver.com>
2024-01-20 22:39:20 +0900

    init

---
+++ src/main/java/dev/gmelon/springstudytest/SpringStudyTestApplication.java
@@ -0,0 +1,13 @@
+package dev.gmelon.springstudytest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringStudyTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringStudyTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/gmelon/springstudytest/SpringStudyTestApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gmelon.springstudytest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudyTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
